cc2d8eb39c69c3e96ec0591feab2651b31c3a62e
Harshit Rawat <harshitr612@gmail.com>
2019-01-27 02:43:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,29 @@ import music
 import utilities
 import google_surf
 import run_executables
+import wave
+import pyaudio
 
 r = sr.Recognizer()
 r.energy_threshold=2500
 r.operation_timeout = 2
 fil_name = "nothisdfdfs"
+
+def play_sound(music_name):
+    chunk = 1024  
+    f = wave.open(r"{}".format(music_name),"rb")  
+    p = pyaudio.PyAudio()  
+    stream = p.open(format = p.get_format_from_width(f.getsampwidth()),  
+                    channels = f.getnchannels(),  
+                    rate = f.getframerate(),  
+                    output = True)  
+    data = f.readframes(chunk)  
+    while data:  
+        stream.write(data)  
+        data = f.readframes(chunk)  
+    stream.stop_stream()  
+    stream.close()  
+    p.terminate() 
 
 while True:
     with sr.Microphone() as source :
@@ -39,6 +57,7 @@ while True:
         elif "bye" in li:
             break
     except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
+    	play_sound('sorry.wav')
+        #print("Google Speech Recognition could not understand audio")
     except sr.RequestError as e:
         print("Could not request results from Google Speech Recognition service; {0}".format(e))
